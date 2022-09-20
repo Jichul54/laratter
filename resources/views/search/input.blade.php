878df92d93331d8a4363200df7a1f33a1c629de8
Jichul54 <kjc778869@gmail.com>
2022-09-20 20:18:17 +0900

    add search, follow

--- resources/views/search/input.blade.php
+++ resources/views/search/input.blade.php
@@ -1,9 +1,9 @@
-<!-- resources/views/tweet/create.blade.php -->
+<!-- resources/views/search/input.blade.php -->
 
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Create New Tweet') }}
+      {{ __('Search Tweet') }}
     </h2>
   </x-slot>
 
@@ -12,18 +12,14 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
           @include('common.errors')
-          <form class="mb-6" action="{{ route('tweet.store') }}" method="POST">
+          <form class="mb-6" action="{{ route('search.result') }}" method="GET">
             @csrf
             <div class="flex flex-col mb-4">
-              <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="tweet">Tweet</label>
-              <input class="border py-2 px-3 text-grey-darkest" type="text" name="tweet" id="tweet">
-            </div>
-            <div class="flex flex-col mb-4">
-              <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="description">Description</label>
-              <input class="border py-2 px-3 text-grey-darkest" type="text" name="description" id="description">
+              <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="tweet">Keyword</label>
+              <input class="border py-2 px-3 text-grey-darkest" type="text" name="keyword" id="keyword">
             </div>
             <button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-              Create
+              Search
             </button>
           </form>
         </div>
